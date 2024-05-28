1618261837e94a00c71b77bfa271c0a02bc2d445
ldb <ldb0316@ictway.co.kr>
2024-05-28 15:34:47 +0900

    Refactor: 불필요파일 삭제

--- src/main/java/com/bbzz/datastandardsvalid/home/repository/TbUserMstRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbzz.datastandardsvalid.home.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bbzz.datastandardsvalid.home.entity.TbUserMstEntity;
-
-public interface TbUserMstRepository extends JpaRepository<TbUserMstEntity, String>{
-
-}
